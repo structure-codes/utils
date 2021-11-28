6016508f25c599d1af209332b0160ccecb0697fc
Alexander Oser <aoser9552@gmail.com>
2021-11-28 13:38:05 -0600

    Fix jest setup

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,6 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  roots: ["<rootDir>"],
+  roots: ["<rootDir>/src"],
   transform: {
     "^.+\\.ts?$": "ts-jest",
   },
